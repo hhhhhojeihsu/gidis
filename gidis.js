13fb334f2277c6e199b1d87c0e218578a81dd81e
HH@Arch@PC <hhhhojeihsu@gmail.com>
2019-03-29 13:37:11 +0800

    Add `help` Command

--- gidis.js
+++ gidis.js
@@ -15,23 +15,28 @@ client.on('message', async msg => {
   if(msg.author.bot) return;
   if(msg.content.substring(0, prefix.length) === prefix) {
     // Send list using pm
-    if(msg.content.substring(prefix.length) === 'list') {
-      msg.author.send(helper.list());
+    if(msg.content.substring(prefix.length) === 'help') {
+      msg.channel.send("Please create an issue on https://github.com/hhhhhojeihsu/gidis");
     }
-    else if(msg.content.substring(prefix.length, 7) === 'list ') {
-      msg.author.send(helper.list(msg.content.substring(7)));
+    else
+    {
+      if(msg.content.substring(prefix.length) === 'list')
+        msg.author.send(helper.list());
+      else if(msg.content.substring(prefix.length, 7) === 'list ') {
+        msg.author.send(helper.list(msg.content.substring(7)));
+      }
+      else {
+        var parsed = helper.parseMsg(msg.content.substring(prefix.length));
+        if(parsed[0] === "")
+          msg.channel.send(parsed[1]);
+        else
+          msg.channel.send(parsed[0]);
+        return;
+      }
+      // Mention sender if not using PM
+      if(msg.channel.type !== 'dm')
+      msg.reply('GI Bot has PMed you the list');
     }
-    else {
-      var parsed = helper.parseMsg(msg.content.substring(prefix.length));
-      if(parsed[0] === "")
-        msg.channel.send(parsed[1]);
-      else
-        msg.channel.send(parsed[0]);
-      return;
-    }
-    // Mention sender if not using PM
-    if(msg.channel.type !== 'dm')
-    msg.reply('GI Bot has PMed you the list');
 	}
 });
 
